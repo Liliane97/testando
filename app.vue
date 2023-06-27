c77eba5601d3ebd36406d36080b85ecb25760f31
liliane97 <lilianebasilio1997@gmail.com>
2023-06-26 21:27:14 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <v-btn>ok</v-btn>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ components/card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <v-btn color="primary">boa noite</v-btn>
+  </div>
+</template>
